4b2b568e3b0a68ccf62272acf376802f1173da2f
DerekKirschbaum <dgkirschbaum@gmail.com>
2022-09-12 21:58:50 -0700

    Made Line Elements/Buttons

--- src/App.js
+++ src/App.js
@@ -1,35 +1,98 @@
 import React from 'react'
 import Line from './Line'
+import BlackLine from './Images/BlackLine.jpg'
+import BlueCrossedLine from './Images/BlueCrossedLine.jpg'
+import RedCrossedLine from './Images/RedCrossedLine.jpg'
+
 
 export default function App () {
 
-  const [lineElements, setLineElements] = React.useState(newLines())
+  const [lines, setLines] = React.useState(newLines())
+
 
   function newLines () {
     const list = []
     for(let i=0; i<15; i++){
       list.push({
         key: i+1,
-        id: i + 1
+        id: i + 1,
+        isCrossed: false,
+        redPlayer: true,
+        src: BlackLine
       })
+    }
+    let count = 0
+    for(let i=1; i<6; i++) {
+      for(let j=0; j<i; j++){
+        list[count].row = i
+        count++
+      }
     }
     return list
   }
 
+  function crossLine (id) {
+    setLines(oldLines => oldLines.map(line => 
+        line.id === id ? 
+          {...line, isCrossed: !line.isCrossed} : 
+          line
+      ))
+  }
+
+  const lineElements = (lines.map(line => 
+    <Line 
+      key={line.id}
+      id={line.id}
+      isCrossed={line.isCrossed}
+      redPlayer={line.redPlayer}
+      src={line.src}
+      row={line.row}
+      crossLine={() => crossLine(line.id)}
+    />
+    ))
+
 
   return (
-    <div className='main'>
-      <div className='header'>
-        <h1 className='title'>Pyramid Nim</h1>
-        <p className='instructions'>Players alternate turns, crossing off as many 
-          adjacent lines in the same row as they want. 
-          The winner is the player that crosses off the last line.
-        </p>
+    <main>
+      <div className='main'>
+        <div className='header'>
+          <h1 className='title'>Pyramid Nim</h1>
+          <p className='instructions'>Players alternate turns, crossing off as many 
+            adjacent lines in the same row as they want. 
+            The winner is the player that crosses off the last line.
+          </p>
+        </div>
+        <div className='gameBox'>
+          <div className='row1'>
+            {lineElements[0]}
+          </div>
+          <div className='row2'>
+            {lineElements[1]}
+            {lineElements[2]}
+
+          </div>
+          <div className='row3'>
+            {lineElements[3]}
+            {lineElements[4]}
+            {lineElements[5]}
+          </div>
+          <div className='row4'>
+            {lineElements[6]}
+            {lineElements[7]}
+            {lineElements[8]}
+            {lineElements[9]}
+          </div>
+          <div className='row5'>
+            {lineElements[10]}
+            {lineElements[11]}
+            {lineElements[12]}
+            {lineElements[13]}
+            {lineElements[14]}
+          </div>
+
+        </div>
+        
       </div>
-      <div className='gameBox'>
-        <Line text={lineElements.id}/>
-      </div>
-      
-    </div>
+    </main>
   );
 };
--- src/Line.js
+++ src/Line.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import BlackLine from './Images/BlackLine.jpg'
+import BlueCrossedLine from './Images/BlueCrossedLine.jpg'
+import RedCrossedLine from './Images/RedCrossedLine.jpg'
+
 
 export default function Line (props) {
+    const imgSrc = props.isCrossed ? RedCrossedLine : BlackLine
+    const x = BlueCrossedLine
+
     return (
-        <div className='line'>
-            <h1>{props.text}</h1>
+        <div className='line' onClick={props.crossLine}>
+                <img className='line-img' src={imgSrc} alt="line"/>
         </div>
     )
 }
